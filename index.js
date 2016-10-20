586b80fce105d49e5f63b020f499e8df9fd398d8
Cinoze <gerinrozecamille@gmail.com>
2016-10-20 07:43:32 +0200

    trying new things

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var Bot = require('slackbots');
+var RankingBot = require('./lib/RankingBot.js');
+var config = require('./config.js').data;
 
 
-var bot = new Bot({
-    token: require('./config.js').slackToken,
-    name: 'cheo'
-})
+
+
+var rankingBot = new RankingBot({token:config.slackToken,name:config.botName});
+
+
+rankingBot.on('start', function() {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':cat:'
+    };
+    console.log("test");
+    // define existing username instead of 'user_name'
+    bot.postMessageToUser('@camille', 'meow!', params);
+
+});
