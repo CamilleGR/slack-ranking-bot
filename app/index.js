b9f48004acaa7ebe3099bdde4740232a116b5b7f
Cinoze <gerinrozecamille@gmail.com>
2016-11-01 23:46:10 +0100

    Add commands to ask cheo displaying stats

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 const reacMng = require('../lib/reactionManager.js')
 const winston = require('winston')
+const commands = require('../lib/commands.js')
 winston.level = process.env.LOG_LEVEL
 
 
@@ -20,7 +21,7 @@ bot.reaction_added(function(data){
     MongoClient.connect(config.dbUrl, function(err, db) {
       winston.debug('NEW REACTION CATCHED IN A LISTENED CHANNEL')
       assert.equal(err,null)
-      reacMng.addReactionPoint(db,data.user,data.reaction)
+      reacMng.addReactionPoint(db,data.reaction,data.user)
       .then(function(userReactionAdded){
         winston.debug('ADD NEW REACTION '+data.reaction+' for user '+data.user+'\nsuccess ? '+userReactionAdded);
       })
@@ -29,8 +30,23 @@ bot.reaction_added(function(data){
   }
 })
 
-/*
-bot.message(function(data){
-console.log(data);
+
+
+//When you send a mesage to cheo
+bot.message(function(data,err){
+    var matches = commands.regex_stat.exec(data.text)
+    if(matches !== null && matches[0] !== ''){
+
+      if(matches[2]==='emoji')
+          MongoClient.connect(config.dbUrl, function(err, db) {
+            reacMng.getReaction(db,matches[3]).then(function(reaction){
+              slack.chat.postMessage(
+                  {token:config.slackToken, channel:data.channel, text:JSON.stringify(reaction), as_user:true}
+                  , function(err,data){
+                    winston.error(err)
+                    winston.debug(data)
+                  })
+            })
+          })
+        }
 })
-*/
