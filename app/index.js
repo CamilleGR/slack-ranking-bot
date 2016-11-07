a2f10f035d51287030c164d483dabee7dbe8cfaf
Cinoze <gerinrozecamille@gmail.com>
2016-11-07 01:11:59 +0100

    REFACTOR to use ES6 notation

--- app/index.js
+++ app/index.js
@@ -8,27 +8,26 @@ const winston = require('winston')
 const commandHandler = require('../lib/commandHandler.js')
 
 
-
 winston.level = process.env.LOG_LEVEL
 
 
 bot.listen({token:config.slackToken});
 
 
-bot.reaction_added(function(data){
+bot.reaction_added((data) => {
   if(data.item.channel == null && config.listenedChannels.indexOf(data.item.channel)<0){
     winston.debug('REACTION CATCHED IN UNLISTENNED CHANNEL')
   }
   else
   {
-    MongoClient.connect(config.dbUrl, function(err, db) {
+    MongoClient.connect(config.dbUrl, (err, db) => {
       winston.debug('NEW REACTION CATCHED IN A LISTENED CHANNEL')
       assert.equal(err,null)
       reacMng.addReactionPoint(db,data.reaction,data.item_user)
-      .then(function(userReactionAdded){
+      .then((userReactionAdded)=>{
         winston.debug('ADD NEW REACTION '+data.reaction+' for user '+data.item_user+'\nsuccess ? '+userReactionAdded);
       })
-      .catch(function(err){winston.log('ERROR',err)})
+      .catch((err)=>{winston.log('ERROR',err)})
     })
   }
 })
@@ -36,7 +35,7 @@ bot.reaction_added(function(data){
 
 
 //When you send a mesage to cheo
-bot.message(function(data,err){
+bot.message((data,err) => {
   if(data != null && data.text != null){
     if(data.text.indexOf('cheo')===0 || data.text.indexOf('Cheo') === 0) commandHandler.handler(data)
     }
