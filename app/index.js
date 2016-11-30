7207f51f3ed6ac5691c515364f5b6e49d6f7714a
Gerin-Roze Camille <gcamille@localhost.localdomain>
2016-12-01 00:19:15 +0100

    correct bug

--- app/index.js
+++ app/index.js
@@ -1,7 +1,4 @@
-//const bot = require('./bot.js')
+const bot = require('./bot.js')
 const cron = require('./cron.js')
 
-console.log(cron.jobs)
-//cron.jobs.forEach(j => j.start());
-cron.jobs[0].start();
-cron.jobs.forEach(t => console.log(t.task))
+cron.jobs.forEach(j => j.start());
