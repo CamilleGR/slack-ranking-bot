fccfff78dcc4a59f27555476e33f09b5b9c5abaf
root <root@vps334572.ovh.net>
2016-12-16 09:47:39 +0100

    Add log on the cron job's definitions

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 const bot = require('./bot.js')
 const cron = require('./cron.js')
 
-cron.jobs.forEach(j => j.start());
+cron.jobs.forEach(j =>{ 
+	console.log("STARTING CRON" + j)
+	j.start()
+});
