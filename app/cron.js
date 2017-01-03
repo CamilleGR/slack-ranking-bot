6ebdc40c82cb21e84e8dba30b5b3e91f1083e527
Gerin-Roze Camille <gcamille@cgerinro-etu-0.etu.utc>
2017-01-03 15:36:33 +0100

    set the path of config.js

--- app/cron.js
+++ app/cron.js
@@ -1,10 +1,10 @@
 const cron = require('node-cron')
-const config = require('../config.js').data
+const config = require('../config/config.js').data
 
 let jobs = []
 
 config.cronJobs.forEach((j) => {
-  let job = cron.schedule(j.cronExpression, require('../cronJobs/'+j.scriptName).execute)
+  let job = cron.schedule(j.cronExpression, require('../config/cronJobs/'+j.scriptName).execute)
   jobs.push(job);
 });
 
